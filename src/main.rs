41ff2d0e0bcc90db10920f47abe921640d9b05a1
tony <antolisah@gmail.com>
2022-01-20 07:40:25 +0300

    variable data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+      Data types
+        i64 - signed integer (64-bit)
+        u64 - unsigned integer (64-bit) - non-negative
+        f32 - float (32-bit)
+    */
+
+    let x: i64 = 45; // i64
+    let f: f32 = 7.34; // f32
+    let b: bool = true; // bool
 }
