100ac2e5be14c49c3200665c5666caf2dc56ba04
tony <antolisah@gmail.com>
2022-01-20 13:09:41 +0300

    basic loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut n = 0;
+
+    loop {
+        n += 1;
+
+        // skip even numbers
+        if n % 2 == 0 {
+            continue;
+        }
+
+        // stop loop at 100
+        if n > 100 {
+            break;
+        }
+        println!("n is {}", n);
+    }
 }
