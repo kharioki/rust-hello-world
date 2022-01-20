2031facbe64f3a82c68187fa4bca1ec6c2471968
tony <antolisah@gmail.com>
2022-01-20 15:13:39 +0300

    loops

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let mut n = 0;
+    let mut m = 1;
 
+    // basic loop
     loop {
         n += 1;
 
@@ -13,6 +15,40 @@ fn main() {
         if n > 100 {
             break;
         }
-        println!("n is {}", n);
+        // println!("n is {}", n);
+    }
+
+    // while loop
+    while m <= 50 {
+        // print m if divisible by 3
+        if m % 3 == 0 {
+            println!("m is {}", m);
+        }
+        m += 1;
+    }
+
+    let numbers = 30..51;
+
+    // for loop
+    for x in 1..11 {
+        // prints 1 to 10 - 11 is non inclusive
+        println!("x is {}", x);
+    }
+
+    // for loop with range
+    for y in numbers {
+        println!("y is {}", y);
+    }
+
+    let animals = vec!["dog", "cat", "bird", "fish"];
+
+    // iterate over vector
+    for a in animals.iter() {
+        println!("animal name is {}", a);
+    }
+
+    // iterate and enumerate
+    for (index, a) in animals.iter().enumerate() {
+        println!("animal name is {} and index is {}", a, index);
     }
 }
