33d71dbd0add49003467b1a17c5b21cc22bce67a
tony <antolisah@gmail.com>
2022-01-20 15:18:28 +0300

    enum types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let player_direction: Direction = Direction::Up;
+
+    match player_direction {
+        Direction::Up => println!("Player is moving up"),
+        Direction::Down => println!("Player is moving down"),
+        Direction::Left => println!("Player is moving left"),
+        Direction::Right => println!("Player is moving right"),
+    }
 }
