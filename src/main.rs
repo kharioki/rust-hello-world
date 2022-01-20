c6de01ad99e3493dbb985e910af0ab20391ef4e2
tony <antolisah@gmail.com>
2022-01-20 17:46:26 +0300

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    print_numbers_to(30);
+}
+
+/*
+  Functions
+*/
+
+fn print_numbers_to(n: u32) {
+    for i in 1..n {
+        if is_even(i) {
+            println!("{} is even!", i);
+        } else {
+            println!("{} is odd!", i);
+        }
+    }
+}
+
+fn is_even(n: u32) -> bool {
+    n % 2 == 0
 }
