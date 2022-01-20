fc2d2af432933864fc607466e5094c7f8306e3c3
tony <antolisah@gmail.com>
2022-01-20 08:58:42 +0300

    if statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 56;
+
+    // it is unnecessary to use parentheses in the `if` keyword
+    if n < 30 {
+        println!("n is less than 30");
+    } else if n == 50 {
+        println!("This is cool!");
+    } else {
+        println!("n is greater than 30, n is {}", n);
+    }
 }
