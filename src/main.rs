1786d618be3ce6f888ff2ecc9a15b420d392b419
tony <antolisah@gmail.com>
2022-01-21 06:44:01 +0300

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 10;
+    {
+        // shadow x in this scope
+        let x = 20;
+    }
+
+    let x = "X is a string";
+    println!("x is {}", x);
+
+    let x = true;
+    println!("x is {}", x);
 }
