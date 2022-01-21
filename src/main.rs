5d0024372a7144017ebe22b0469e7d34a0ed5925
tony <antolisah@gmail.com>
2022-01-21 05:35:47 +0300

    code blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 10;
+
+    {
+        // isolated scope
+        let y = 20;
+        println!("x: {}, y: {}", x, y);
+    }
+
+    // y is out of scope
 }
