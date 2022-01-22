5955d471420a14d85daaf6e876bf252b225b08ba
tony <antolisah@gmail.com>
2022-01-22 05:26:22 +0300

    references

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+
+    // immutable references
+    let xr = &x;
+
+    {
+        // mutable references
+        let xm = &mut x;
+
+        *xm += 1;
+
+        println!("x is {}", *xm);
+    }
+    println!("x is {}", x);
 }
