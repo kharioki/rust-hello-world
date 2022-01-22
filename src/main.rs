7621989e49655cebc096d92ce1b0faef95bc11e8
tony <antolisah@gmail.com>
2022-01-22 05:48:28 +0300

    tuple structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// structs - structs are used to group data together to form a single unit
+// structs are always prefixed with the keyword struct
+// structs are used to define a data type
+struct Color {
+    red: u8, // u8: 0-255
+    green: u8,
+    blue: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut bg = Color {
+        red: 255,
+        green: 70,
+        blue: 15,
+    };
+
+    bg.blue = 0;
+
+    println!("{}, {}, {}", bg.red, bg.green, bg.blue);
 }
