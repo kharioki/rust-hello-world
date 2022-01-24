ae31300684e80a855b9aaa4f2dcff544be22fcdc
tony <antolisah@gmail.com>
2022-01-24 11:19:56 +0300

    impl keyword

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
 /*
-    Arrays:
-    Arrays are a collection of values of the same type.
-    Arrays are zero-indexed.
+    impl:
+    impl is a keyword in Rust. It is used to add methods in a struct.
 */
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // &self - reference to the current instance of the struct
+
+    fn print_description(&self) {
+        println!(
+            "Rectangle with width {} and height {}",
+            self.width, self.height
+        );
+    }
+
+    fn is_square(&self) -> bool {
+        self.width == self.height
+    }
+}
+
 fn main() {
-    // you can specify the type of the elements in the array and the length - not required
-    // let numbers: [i32; 5] = [1, 2, 3, 4, 5];
-    let numbers = [1, 2, 3, 4, 5];
+    let my_rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
-    // you can also declare an array by passing the value and length
-    // let numbers = [2; 400];
-
-    // Accessing an element
-    // numbers[0]; // 1
-    // numbers[4]; // 5
-
-    // looping through an array
-    for number in numbers.iter() {
-        println!("{}", number);
-    }
-
-    // looping through an array with indices
-    for i in 0..numbers.len() {
-        println!("{}", numbers[i]);
-    }
+    my_rect.print_description();
+    println!("Is Rectangle a square? {}", my_rect.is_square());
 }
