fd37190914af8da0174bf5733d4d1a2094fd344f
tony <antolisah@gmail.com>
2022-01-24 21:09:15 +0300

    string methods

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,58 @@
-extern crate rand;
-use rand::Rng;
+/* String methods */
 
 fn main() {
-    let num = rand::thread_rng().gen_range(1..101); // Generate a random number between 1 and 100
-    println!("Random number! {}", num);
+    /* Replace */
 
-    // flip a coin
-    let coin = rand::thread_rng().gen_bool(1.0 / 2.0); // Generate a random boolean
-    println!("Coin flip! {}", coin);
+    {
+        let my_string = String::from("Hey Tony");
+        println!("{}", my_string.replace("Tony", "Kharioki"));
+    }
+
+    /* Lines - split string on lines */
+
+    {
+        let my_string = String::from("Hey Tony\nI am a Rustacean");
+        for line in my_string.lines() {
+            println!("{}", line);
+        }
+    }
+
+    /* Split */
+
+    {
+        let my_string = String::from("I+Am+A+Rustacean");
+        // split and return vector
+        let split_string: Vec<&str> = my_string.split("+").collect();
+
+        for word in split_string {
+            println!("{}", word);
+        }
+        // let mut iter = my_string.split("+");
+        // println!("{}", iter.next().unwrap());
+        // println!("{}", iter.next().unwrap());
+        // println!("{}", iter.next().unwrap());
+        // println!("{}", iter.next().unwrap());
+    }
+
+    /* Trim */
+
+    {
+        let my_string = String::from("  Hey Tony \n\r ");
+        println!("{}", my_string.trim());
+    }
+
+    /* Chars */
+
+    {
+        let my_string = String::from("Hey Tony");
+        // using loop
+        for c in my_string.chars() {
+            println!("{}", c);
+        }
+        // get character with match operator
+        match my_string.chars().nth(6) {
+            Some(c) => println!("{}", c),
+            None => println!("No character found"),
+        }
+    }
 }
