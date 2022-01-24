b073e56422c4b7620d01cabaadd747e429cb9f8b
tony <antolisah@gmail.com>
2022-01-24 09:00:27 +0300

    defining arrays

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@
 */
 
 fn main() {
+    // you can specify the type of the elements in the array and the length - not required
+    // let numbers: [i32; 5] = [1, 2, 3, 4, 5];
     let numbers = [1, 2, 3, 4, 5];
+
+    // you can also declare an array by passing the value and length
+    // let numbers = [2; 400];
 
     // Accessing an element
     // numbers[0]; // 1
