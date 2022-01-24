63d06d72804cf017df9ea255fbe5c8877a0337d7
tony <antolisah@gmail.com>
2022-01-24 06:58:25 +0300

    tuple structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+struct Color(u8, u8, u8);
+
 fn main() {
-    println!("Hello, world!");
+    let mut red = Color(255, 0, 0);
+
+    red.2 = 58;
+
+    println!("red is {}, {}, {}", red.0, red.1, red.2);
 }
