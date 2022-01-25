da6911d01136da2a6786834a7112bbf3611b9f93
tony <antolisah@gmail.com>
2022-01-25 14:01:43 +0300

    parsing json

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,56 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+use serde_json::Value as JsonValue;
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
 }
 
-impl Rectangle {
-    // check if a rectangle can fit in another rectangle
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
+fn main() {
+    let json_str = r#"
+        {
+            "name": "Kharioki Tony",
+            "age": 30,
+            "phones": [
+                "+254 729918514",
+                "+254 738754710"
+            ]
+        }
+    "#;
 
-fn main() {}
+    // let json: JsonValue = serde_json::from_str(json_str).unwrap();
+    // println!(
+    //     "Please call {} at the number {}",
+    //     json["name"], json["phones"][0]
+    // );
 
-fn give_seven() -> i32 {
-    7
-}
+    let res = serde_json::from_str::<Person>(json_str);
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    // if res.is_ok() {
+    //     let json: JsonValue = res.unwrap();
+    //     println!(
+    //         "Please call {} at the number {} or {}",
+    //         json["name"].as_str().unwrap(),
+    //         json["phones"][0].as_str().unwrap(),
+    //         json["phones"][1].as_str().unwrap()
+    //     );
+    // } else {
+    //     println!("{}", res.unwrap_err());
+    // }
 
-    // simple assert
-    #[test]
-    fn assert_test() {
-        assert!(4 == 4);
-    }
-
-    // assertion equals
-    #[test]
-    #[ignore] // ignore this test
-    fn assert_eq_test() {
-        assert_eq!(2 + 2, 4); // equal - should pass
-        assert_ne!(2 + 2, 5); // not equal - should pass
-    }
-
-    #[test]
-    #[should_panic]
-    fn another() {
-        panic!("Make this test fail");
-    }
-
-    #[test]
-    fn test_fn() {
-        assert_eq!(super::give_seven(), 7);
-    }
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let rect1 = Rectangle {
-            width: 30,
-            height: 50,
-        };
-        let rect2 = Rectangle {
-            width: 10,
-            height: 40,
-        };
-        let rect3 = Rectangle {
-            width: 60,
-            height: 45,
-        };
-        assert!(rect1.can_hold(&rect2));
-        assert!(!rect1.can_hold(&rect3));
+    if res.is_ok() {
+        let person: Person = res.unwrap();
+        println!(
+            "Please call {} at the number {} or {}",
+            person.name, person.phones[0], person.phones[1]
+        );
+    } else {
+        println!("Sorry we could not parse the json");
     }
 }
